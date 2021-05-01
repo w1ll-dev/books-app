2fb53496e6d50e58fac17b0ddf55db4a983e8a69
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-01 16:34:22 -0300

    add protocol to Book

---
+++ src/repository/protocols/book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  id: number;
+  title: string;
+  subtitle?: string;
+  authors?: [string];
+  description?: string;
+  imageLinks?: { smallThumbnail: string; thumbnail: string };
+}
