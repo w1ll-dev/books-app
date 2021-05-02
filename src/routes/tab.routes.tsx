84b7ad1ec368873cf32d29d481e9b59324587a02
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:50:52 -0300

    add tab routes

---
+++ src/routes/tab.routes.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { colors } from "../styles/colors";
+import { Feather } from "@expo/vector-icons";
+import { Home, Details, User } from "../pages";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export const TabRoutes = () => (
+  <Navigator
+    tabBarOptions={{
+      activeTintColor: colors.black,
+      inactiveTintColor: colors.grey,
+      style: { height: 59, padding: 10 },
+    }}
+  >
+    <Screen
+      name={"Home"}
+      component={Home}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <Feather size={size} color={color} name={"home"} />
+        ),
+      }}
+    />
+    <Screen
+      name={"Details"}
+      component={Details}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <Feather size={size} color={color} name={"book"} />
+        ),
+      }}
+    />
+    <Screen
+      name={"User"}
+      component={User}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <Feather size={size} color={color} name={"user"} />
+        ),
+      }}
+    />
+  </Navigator>
+);
