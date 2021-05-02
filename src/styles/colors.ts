55a903bb7739367247d89543691a08340679a95e
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:32:10 -0300

    create colors config file

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  beige: "#F2F2F2",
+  lightBeige: "#FEFEFE",
+  cream: "#EEF5DB",
+  white: "#FFFFFF",
+  lightPink: "#ff6978",
+  darkGrey: "#3F4043",
+  lightGrey: "#E7E7E1",
+  concrete: "#74776D",
+  grey: "#BFBEBF",
+  lightBlue: "#4ABDF1",
+  darkBlue: "#00173D",
+  purple: "#9013fe",
+  dark: "#2A2B26",
+  black: "#000000",
+};
