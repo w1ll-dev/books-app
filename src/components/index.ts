6fc356b01e3c533163cd40402314662865491387
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:47:00 -0300

    create export file to components folder

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import { DiscoverBookCard } from "./DiscoverBookCard";
+import { CurrentlyReadingCard } from "./CurrentlyReadingCard";
+import { ListTitle } from "./ListTitle";
+import { WelcomeMessage } from "./WelcomeMessage";
+
+export { DiscoverBookCard, ListTitle, WelcomeMessage, CurrentlyReadingCard };
