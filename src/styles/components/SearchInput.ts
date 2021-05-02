2febf818686a6f0931a92ac977245f7dbba9d546
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:36:56 -0300

    create search input styles

---
+++ src/styles/components/SearchInput.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { colors } from "../colors";
+import { fonts } from "../fonts";
+
+const statusBarHeight = getStatusBarHeight();
+
+export const SearchInput = styled.TextInput`
+  width: 100%;
+  height: 48px;
+  margin-top: ${statusBarHeight + 50}px;
+  padding: 0 16px;
+  background: ${colors.white};
+  align-items: flex-start;
+  border-radius: 10px;
+  font-family: ${fonts.title};
+  font-size: 16px;
+  padding-left: 41px;
+`;
