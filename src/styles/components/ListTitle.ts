3557912ebb1ae3d162538aaae979ddcd1d9009fc
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:37:22 -0300

    create list title component style

---
+++ src/styles/components/ListTitle.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import { Container, CommonText } from "../Common";
+
+export const Wrapper = styled(Container)`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 15px;
+`;
+
+export const Title = styled(CommonText)`
+  font-size: 18px;
+  color: ${colors.darkGrey};
+`;
+
+export const ButtonTitle = styled(CommonText)`
+  font-size: 14px;
+  color: ${colors.lightBlue};
+`;
