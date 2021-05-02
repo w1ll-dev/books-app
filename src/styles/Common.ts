e44c36d659fd13334744e124892fa46132e3ae04
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:35:50 -0300

    create common styled components

---
+++ src/styles/Common.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+import { fonts } from "./fonts";
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const CommonText = styled.Text`
+  font-family: ${fonts.title};
+`;
