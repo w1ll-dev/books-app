e86483e2beb01faf5865c21d704e5bed4b56c400
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:45:36 -0300

    create discover book component

---
+++ src/components/DiscoverBookCard.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Wrapper,
+  BookNameContainer,
+  BookTitle,
+  BookImage,
+  BookAuthor,
+  StatsContainer,
+  ReadStatsText,
+  ReadStatsValue,
+} from "../styles/components/DiscoverBookCard";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "../styles/colors";
+import { Container } from "../styles/Common";
+import { OvalSvg, RectangleSvg, CircleSvg, TriangleSvg } from "./svg";
+
+interface DiscoverBookCardProps {
+  title: string;
+  author?: string;
+  imageUri?: string;
+  readStats?: string;
+}
+
+export function DiscoverBookCard({
+  title,
+  author,
+  imageUri,
+  readStats,
+}: DiscoverBookCardProps) {
+  return (
+    <>
+    <Wrapper>
+      <OvalSvg />
+      <BookNameContainer>
+        <Container>
+          <BookTitle>{title}</BookTitle>
+          <BookAuthor>{author}</BookAuthor>
+        </Container>
+        {readStats && (
+          <StatsContainer>
+            <MaterialIcons
+              name="insert-chart-outlined"
+              size={20}
+              color={colors.grey}
+            />
+            <ReadStatsValue>{`${readStats} `}</ReadStatsValue>
+            <ReadStatsText>Read Now</ReadStatsText>
+          </StatsContainer>
+        )}
+      </BookNameContainer>
+      <BookImage source={{ uri: imageUri }} />
+      <RectangleSvg discoverBooksList={true}/>
+      <TriangleSvg />
+      <CircleSvg discoverBooksList={true} />
+    </Wrapper>
+    </>
+  );
+}
