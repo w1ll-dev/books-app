d14c55dcfdac5a29db93684f557907a61b0d41e6
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-02 18:44:47 -0300

    add export file to svg components folder

---
+++ src/components/svg/index.ts
@@ -0,0 +1,6 @@
+import { OvalSvg } from "./ovalSvg";
+import { RectangleSvg } from "./rectangleSvg";
+import { CircleSvg } from "./circleSvg";
+import { TriangleSvg } from "./triangleSvg";
+
+export { OvalSvg, RectangleSvg, CircleSvg, TriangleSvg };
