7fccdb53fe90893ba0255dce59a44129206320ff
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-03 20:46:42 -0300

    add border radius to reviews of the days background

--- src/styles/components/ReviewsOfTheDaysCard.ts
+++ src/styles/components/ReviewsOfTheDaysCard.ts
@@ -1,10 +1,12 @@
 import styled from "styled-components/native";
 import { Container } from "../Common";
 
-export const Wrapper = styled(Container)``
+export const Wrapper = styled(Container)``;
 
 export const BackgroundImage = styled.Image.attrs((props) => ({
-  source: require('../../assets/images/background.png'),
+  source: require("../../assets/images/background.png"),
 }))`
   width: 100%;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
 `;
