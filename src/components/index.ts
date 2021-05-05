de51d6a9b5f15cdb98ae8584280754ab01d42c83
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-04 21:25:07 -0300

    export book card component by export file

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,12 @@ import { DiscoverBookCard } from "./DiscoverBookCard";
 import { CurrentlyReadingCard } from "./CurrentlyReadingCard";
 import { ListTitle } from "./ListTitle";
 import { WelcomeMessage } from "./WelcomeMessage";
+import { BookCard } from "./BookCard";
 
-export { DiscoverBookCard, ListTitle, WelcomeMessage, CurrentlyReadingCard };
+export {
+  DiscoverBookCard,
+  ListTitle,
+  WelcomeMessage,
+  CurrentlyReadingCard,
+  BookCard,
+};
