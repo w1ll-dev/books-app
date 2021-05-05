7fee41f2fa99bd161517b3c47093fb787d9a6cb8
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-05 19:35:17 -0300

    add currently reading container, create load books button and load  books button label

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { Container } from "../Common";
 import { colors } from "../colors";
+import { fonts } from "../fonts";
 
 export const Wrapper = styled(Container)`
   background: ${colors.beige};
@@ -10,14 +11,25 @@ export const HomeListContainer = styled.View`
   margin: 0 20px 30px;
 `;
 
-export const CurrentlyReadingContainer = styled.View`
-  margin: 0 0 30px;
-`;
-
 export const DiscoverBookListContainer = styled.View`
   margin-right: -20px;
 `;
 
 export const BooksListContainer = styled(HomeListContainer)`
   flex: 1;
+`;
+
+export const LoadBooksButton = styled.TouchableOpacity`
+  margin: 15px 0 10px;
+  padding: 10px;
+  background: ${colors.darkGrey};
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+export const LoadBooksButtonLabel = styled.Text`
+  font-family: ${fonts.currentlyReadingAuthor};
+  font-size: 15px;
+  color: ${colors.white};
 `;
