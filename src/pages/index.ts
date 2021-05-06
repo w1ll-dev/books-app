a398d73bcf670ec70d84e650256322dfb811ef47
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-06 19:04:19 -0300

    export books search page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import { Home } from "./Home";
 import { Details } from "./Details";
 import { Libraries } from "./Libraries";
 import { Profile } from "./Profile";
+import { BooksSearch } from "./BooksSearch";
 
-export { Home, Details, Profile, Libraries };
+export { Home, Details, Profile, Libraries, BooksSearch };
