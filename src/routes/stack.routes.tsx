8c49d1f8fc0bbb5b937c22540c57d024ce4f14ea
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-06 19:12:19 -0300

    make stack routes show tab routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Details, Home } from "../pages";
+import { Details } from "../pages";
+import { TabRoutes} from './tab.routes'
 import { colors } from "../styles/colors";
+import HomeStackRoutes from "./home.stack.routes";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,8 +15,9 @@ const StackRoutes = () => (
         backgroundColor: colors.white,
       },
     }}
+    
   >
-    <Screen name="Home" component={Home} />
+    <Screen name="Home" component={TabRoutes} />
     <Screen name="Details" component={Details} />
   </Navigator>
 );
