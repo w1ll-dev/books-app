ec2ccad1004b52c19159846af4d0c1d39d8e2542
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-06 07:31:52 -0300

    add onSelected method to book card

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -10,11 +10,12 @@ interface BookCardProps {
   bookImageUri?: string;
   bookTitle: string;
   bookAuthor: string;
+  onSelected: any
 }
 
 export const BookCard = memo(
-  ({ bookImageUri, bookTitle, bookAuthor }: BookCardProps) => (
-    <Wrapper>
+  ({ bookImageUri, bookTitle, bookAuthor, onSelected }: BookCardProps) => (
+    <Wrapper onPress={onSelected}>
       <BookImage source={{ uri: bookImageUri }} />
       <BookTitle>{bookTitle}</BookTitle>
       <BookAuthor>{bookAuthor}</BookAuthor>
