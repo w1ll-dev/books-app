b135423bb1183d6f9d10d314a8854bb7f5e4b473
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-06 10:47:50 -0300

    create global state file

---
+++ src/context/globalState.tsx
@@ -0,0 +1,14 @@
+// import { createContext, useContext } from "react";
+// import { Book } from "../repository/protocols";
+
+// export type BooksContextType = {
+//   books: Book[];
+//   setTheme: () => void;
+// };
+
+// export const ThemeContext = createContext<BooksContextType>({
+//   theme: Theme.Dark,
+//   setTheme: (theme) => console.warn("no theme provider"),
+// });
+
+// export const useTheme = () => useContext(ThemeContext);
