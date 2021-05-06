f2ffe8a1e34940c8844be52ea372da1cd896d677
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-05 21:45:27 -0300

    add wrapper to search input and search icon

--- src/styles/components/SearchInput.ts
+++ src/styles/components/SearchInput.ts
@@ -2,13 +2,12 @@ import styled from "styled-components/native";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { colors } from "../colors";
 import { fonts } from "../fonts";
+import { Ionicons } from "@expo/vector-icons";
 
 const statusBarHeight = getStatusBarHeight();
 
-export const SearchInput = styled.TextInput`
+export const StyledSearchInput = styled.TextInput`
   height: 48px;
-  margin: ${statusBarHeight + 50}px 20px 0px;
-  padding: 0 16px;
   color: ${colors.black};
   background: ${colors.white};
   align-items: flex-start;
@@ -17,5 +16,20 @@ export const SearchInput = styled.TextInput`
   font-weight: bold;
   font-size: 16px;
   padding-left: 41px;
+`;
+
+export const Wrapper = styled.View`
+  margin: ${statusBarHeight + 50}px 20px 0px;
   margin-bottom: 30px;
+  justify-content: center;
+`;
+
+export const SearchIcon = styled(Ionicons).attrs(() => ({
+  name: "search-outline",
+  size: 18,
+  color: colors.grey,
+}))`
+  position: absolute;
+  transform: scaleX(-1);
+  margin-left: 15px;
 `;
