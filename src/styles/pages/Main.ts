db192d2960d6d500f0adde9198562c79cb8afb45
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-05-06 19:11:45 -0300

    create main styles

---
+++ src/styles/pages/Main.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { colors } from "../colors";
+import { Container } from "../Common";
+
+export const Wrapper = styled(Container)`
+  background: ${colors.beige};
+`;
